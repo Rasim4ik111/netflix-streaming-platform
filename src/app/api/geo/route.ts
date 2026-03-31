14d58e720b0a670690627d69cc5fbd2359acd4d1
mirrasim <mirrasim@mirrasims-MacBook-Pro.local>
2026-03-31 10:28:01 +0400

    created logic for indetify location of user

---
+++ src/app/api/geo/route.ts
@@ -0,0 +1,24 @@
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+  const country = request.headers.get("x-vercel-ip-country") ?? "AZ";
+  const city = request.headers.get("x-vercel-ip-city") ?? "Baku";
+  const region = request.headers.get("x-vercel-ip-region") ?? "BA";
+  let nearestServer;
+
+  if (country === "US" || country === "CA") {
+    nearestServer = "Americas";
+  } else if (country === "AZ" || country === "TR") {
+    nearestServer = "Europe";
+  } else {
+    nearestServer = "Asia";
+  }
+
+  return Response.json({
+    country,
+    city,
+    region,
+    nearestServer,
+    timestamp: new Date().toISOString(),
+  });
+}
